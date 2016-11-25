891adcaba53d44f2f6566d427984951605d13c45
Sebastien Bouchard <jbijjer@gmail.com>
2016-11-25 00:48:15 -0500

    Ajout Loading + Style sur Bouton

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, LoadingController } from 'ionic-angular';
 
 
 @Component({
@@ -8,14 +8,14 @@ import { NavController, AlertController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
 
   }
-
+  //N'est plus vraiment utilisée... parce que y a sûrement un autre moyen
   showAlert() {
     let alert = this.alertCtrl.create({
       title: 'New checklist',
-      message: 'Hello sir!',
+      message: 'Hello sir!',      
       buttons: [
         {
           text: 'Go away Alfred!'
@@ -23,6 +23,26 @@ export class HomePage {
       ]
     });
     alert.present();
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Ouin... On vérifie de quoi...",
+      duration: 5000
+    });
+    let alert = this.alertCtrl.create({
+      title: 'CONFIRMATION!!!',
+      message: 'Marie-Eve Lemire est cute en criss!',      
+      buttons: [
+        { 
+          text: 'Fermer'          
+        }
+      ]
+    });
+    loader.present();
+    setTimeout(function(){
+      alert.present()
+    },5000);
   }
 
 }
