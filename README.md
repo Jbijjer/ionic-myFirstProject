b7376d101b0b62c7e5e6c6a3468d5c2da6c8150d
Sebastien Bouchard <jbijjer@gmail.com>
2016-11-24 23:31:55 -0500

    First Button created

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ionic-myFirstProject
-https://www.udemy.com/ionic-2-tutorial/learn/v4/t/lecture/5096078
+First project created from this lecture -->  https://www.udemy.com/ionic-2-tutorial/learn/v4/t/lecture/5096078
---
+++ src/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'New checklist',
+      message: 'Hello sir!',
+      buttons: [
+        {
+          text: 'Go away Alfred!'
+        }
+      ]
+    });
+    alert.present();
+  }
+
+}
